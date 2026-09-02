004b4cebf7d8bd03da5a7e680f26a9d5482bc8e9
David-Orizu31 <70930479+David-Orizu31@users.noreply.github.com>
2026-09-02 23:22:51 +0100

    Backend Model Migration and Controllers

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['title', 'slug', 'is_published', 'body'];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\Frontend\WelcomeController;
+use App\Http\Controllers\Frontend\PostShowController;
+
+Route::get('/', WelcomeController::class);
+Route::get('/posts/{post:slug}', PostShowController::class);
+
+Route::apiResource('dashboard/posts', PostController::class)
+    ->middleware(['auth:sanctum'])
+    ->except(['create', 'edit']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -10,4 +20,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::get('/users', function (Request $request) {
     return User::all();
+});
+
+Route::get('/403', function (Request $request) {
+    return response()->json('unauthorized', 403);
 });
