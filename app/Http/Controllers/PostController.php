f7e7b0adb82312ac13aa65126911045b78a9da15
David-Orizu31 <70930479+David-Orizu31@users.noreply.github.com>
2026-09-03 01:18:18 +0100

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = auth()->user()->posts()->orderBy('created_at','desc')->get();
+        return response()->json($posts);
     }
 
     /**
